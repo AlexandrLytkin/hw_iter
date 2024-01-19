46335791fe94bd37bbe6db238eece3df146e5e59
AlexandrLytkin <111504959+AlexandrLytkin@users.noreply.github.com>
2024-01-19 23:39:15 +0300

    Исправил

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 class EvenNumbers:
 
-    def __init__(self, n, start=0, end=1):
-        self.i = 0
-        self.a = start
-        self.b = end
-        self.n = n
+    def __init__(self, start=0, end=1):
+        self.start = start
+        self.end = end
+        self.cont = 0
 
     def __iter__(self):
-        self.i = self.i
-        self.a, self.b = self.a, self.b
+        self.cont = 0
+        self.start, self.end = self.start, self.end
         return self
 
     def __next__(self):
-        self.i += 1
-        if self.i > 1:
-            if self.i > self.n:
-                raise StopIteration
-            self.a, self.b = self.b, self.a + self.b
-        return self.a
+        if self.start > self.end:
+            raise StopIteration
+        elif self.start % 2 == 0:
+            self.cont = self.start
+            self.start += 1
+        self.start += 1
+        return self.cont
 
 
-evenNum = EvenNumbers(n=10)
-for i in evenNum:
+en = EvenNumbers(start=10, end=25)
+for i in en:
     print(i)
-
-evenNum2 = EvenNumbers(start=10, end=25, n=10)
-listNum = list(map(int, evenNum2))
-print(listNum)
